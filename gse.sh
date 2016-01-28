bb5386e70d5dc5ca89b25d0fe18a7f83208de3e6
Andy Carlson <carlsoan@dhcp-10-169-4-19.uhih.uc.edu>
2016-01-28 11:11:53 -0500

    Finished command line script

--- gse.sh
+++ gse.sh
@@ -4,14 +4,18 @@ read_authtoken() {
 	cat gse.conf | grep "^authtoken " | sed 's/^authtoken //g'
 }
 
-if [ -z "$1" ]; then
-	echo "usage: gse.sh <hostname> <path> <redirect-url>"
+show_usage() {
+	echo "usage: gse.sh <hostname> <path> <redirect-url>"	
+}
+
+if [ "$#" -ne 3 ]; then
+	show_usage
 else
 	authtoken=$(read_authtoken)
-	success=$(curl -s -X POST -H GSE_path:$2 -H GSE_url:$3 -H GSE_authtoken:$authtoken http://$1$2 | grep '<title>Redirecting...</title>')
+	success=$(curl -s -X POST -H GSE_path:$2 -H GSE_url:$3 -H GSE_authtoken:$authtoken http://$1$2 | grep '<title>Redirecting...</title>')	
 	if [ -z "$success" ]; then
-		echo "Error adding redirect"
+		echo "Error adding redirect (3)"
 	else
-		echo "Added redirect"
+		echo "Redirect added"
 	fi
 fi
